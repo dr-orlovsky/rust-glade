9405c23b9ba7090fb611b9628ca71302e5989e52
Dr. Maxim Orlovsky <dr.orlovsky@gmail.com>
2020-06-13 21:38:50 +0200

    Crate setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+// Derive macros for simple use of GTK+ Glade UI bindings in Rust
+//
+// Written in 2020 by
+//     Dr. Maxim Orlovsky <dr.orlovsky@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the MIT License
+// along with this software.
+// If not, see <https://opensource.org/licenses/MIT>.
+
 #[cfg(test)]
 mod tests {
     #[test]
